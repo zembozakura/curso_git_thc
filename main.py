0ea36e19f446deffebedb766d79faf65b16e36d1
The Harry Code <theharrycode@gmail.com>
2024-05-20 12:49:48 -0500

    Se agrega campo direccion

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ print("Su nombre es "+name)
 #Carlos hizo esto
 lastname = input("Ingrese su apellido")
 print("Su apellido es "+lastname)
+
+#The Harry Code hizo esto
+direccion = input("Ingrese su direccion")
+print("Su Direccion es "+direccion)
+
+#Debería continuar con el curso de Pyhton?
